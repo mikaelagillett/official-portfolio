85140995c2e070dc5a27a38a600c00b3678042bc
mikaelagillett <mikaelaihgillett@gmail.com>
2023-10-02 14:28:02 -0400

    Initial creation react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mikaela Gillett</h1>
+        <h2>front end developer</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
