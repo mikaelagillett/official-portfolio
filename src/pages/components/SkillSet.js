95f4b810841338b8a2b092b488a79720be46e098
mikaelagillett <mikaelaihgillett@gmail.com>
2023-10-03 15:32:33 -0400

    Edited skillset and project highlights to make responsive
    added description to skills

--- src/pages/components/SkillSet.js
+++ src/pages/components/SkillSet.js
@@ -1,22 +1,42 @@
 import "./SkillSet.css";
 
-import reactIcon from "../../assets/react-icon.png";
-import responsiveIcon from "../../assets/responsive-icon.png";
-import apiIcon from "../../assets/api-icon.png";
-
 function SkillSet() {
   return (
     <section className="skill-set">
       <h3>skill set</h3>
       <div className="skill-icons">
-        <i class="bi bi-filetype-css"></i>
-        <i class="bi bi-filetype-html"></i>
-        <i class="bi bi-filetype-js"></i>
-        <i class="bi bi-filetype-jsx"></i>
-        <i class="bi bi-tablet"></i>
-        <i class="bi bi-bootstrap"></i>
-        <i class="bi bi-cloud-download"></i>
-        <i class="bi bi-palette2"></i>
+        <div className="d-flex flex-column">
+          <i className="bi bi-filetype-css"></i>
+          <span>css styling language</span>
+        </div>
+        <div className="d-flex flex-column">
+          <i className="bi bi-filetype-html"></i>
+          <span>html language</span>
+        </div>
+        <div className="d-flex flex-column">
+          <i className="bi bi-filetype-js"></i>
+          <span>java script language</span>
+        </div>
+        <div className="d-flex flex-column">
+          <i className="bi bi-filetype-jsx"></i>
+          <span>react development</span>
+        </div>
+        <div className="d-flex flex-column">
+          <i className="bi bi-tablet"></i>
+          <span>responsive development</span>
+        </div>
+        <div className="d-flex flex-column">
+          <i className="bi bi-bootstrap"></i>
+          <span>bootstrap framework</span>
+        </div>
+        <div className="d-flex flex-column">
+          <i className="bi bi-cloud-download"></i>
+          <span>api integration</span>
+        </div>
+        <div className="d-flex flex-column">
+          <i className="bi bi-palette2"></i>
+          <span>colour theory</span>
+        </div>
       </div>
     </section>
   );
